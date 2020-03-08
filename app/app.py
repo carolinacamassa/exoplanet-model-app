2ec1b20553ffa380c288af898aac3a871cf57e7f
Carolina Camassa <hissi@hotmail.it>
2020-03-08 22:44:18 +0100

    removed P_PERIOD feature

--- app/app.py
+++ app/app.py
@@ -26,21 +26,26 @@ def startup():
     score = [0]
     rforest = load("model/model.joblib")
 
+# model related variables
+features = ['P_RADIUS_EST',
+			 'P_MASS_EST',
+			 'P_DISTANCE_EFF',
+			 'S_RADIUS_EST',
+			 'S_MAG']
 
 
 # form to collect user input:
 class InputForm(FlaskForm):
-    default_val = [10, 1,30,0.1,1,5]
+    default_val = [10, 1,0.1,1,5]
     p_radius = DecimalRangeField('Radius', default =  default_val[0] )
     p_mass = DecimalRangeField('Mass', default = default_val[1] )
-    p_period = DecimalRangeField('Period', default = default_val[2]  )
-    p_distance = DecimalRangeField('Effective th. distance from star', default = default_val[3]  )
-    s_radius = DecimalRangeField('Radius',default =  default_val[4] )
-    s_mag = DecimalRangeField('Magnitude',default =  default_val[5]  )
+    p_distance = DecimalRangeField('Effective th. distance from star', default = default_val[2]  )
+    s_radius = DecimalRangeField('Radius',default =  default_val[3] )
+    s_mag = DecimalRangeField('Magnitude',default =  default_val[4]  )
     submit = SubmitField('Submit')
 
 
-# Load the serialized ML model
+
 
 
 @app.route("/", methods=['POST', 'GET'])
@@ -51,23 +56,10 @@ def index():
     probability = 100
     if form.validate_on_submit():
         x_user = pd.DataFrame([[form.p_radius.data,
-    		form.p_mass.data, form.p_period.data, form.p_distance.data,
+    		form.p_mass.data, form.p_distance.data,
             form.s_radius.data,form.s_mag.data]], columns = features)
         score = rforest.predict(x_user[features])
         probs =  rforest.predict_proba(x_user[features])
         probability = np.max(probs)*100
         print(probs, file = sys.stderr)
     return render_template('index.html', form=form, prob = probability ,hab_score = score[0])
-
-
-
-
-
-
-# model related variables
-features = ['P_RADIUS_EST',
-			 'P_MASS_EST',
-			 'P_PERIOD',
-			 'P_DISTANCE_EFF',
-			 'S_RADIUS_EST',
-			 'S_MAG']
